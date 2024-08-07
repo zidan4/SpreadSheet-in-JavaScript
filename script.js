2e99126cceb7019b52a5bb544bddcb068c669583
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-07 20:42:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 }
 
-
 const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
 
 const highPrecedence = str => {
